bab5dcb09c3b71cfde0c8baab845d1f6a54aed64
TheFox <github@fox21.at>
2010-09-23 14:09:16 +0200

    Backup shellscript

---
+++ bak.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Created @ 12.10.2009 by TheFox@fox21.at
+
+NEWFILE="backup_`date +"%Y-%m-%d_%H-%M-%S"`.tar.gz"
+if [ "x$1" != "x" ] && [ -f $1 ]; then
+	MMV="mv $1 $NEWFILE"
+	echo $MMV
+	$MMV
+fi
+
+# EOF
